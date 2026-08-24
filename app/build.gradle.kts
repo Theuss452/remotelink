195029e72e1e8e002d56750a747474811e28af31
Theuss452 <matheushsaz05@gmail.com>
2026-08-23 23:34:25 -0300

    Bump RemoteLink to v0.3 alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "app.remotelink"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2.0-alpha"
+        versionCode = 3
+        versionName = "0.3.0-alpha"
     }
 
     buildFeatures { buildConfig = true }
